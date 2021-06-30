249d20a6173c71b828c016db4c0ff8b579441ac7
Mishka Tenieshvili <59793581+Mishkaten@users.noreply.github.com>
2021-07-01 01:14:47 +0400

    Create 2b_shifter.v

---
+++ Final_Prep/2b_shifter.v
@@ -0,0 +1,37 @@
+module shifter (
+    input s, 
+    input [1:0]inp,
+    output [1:0]out
+);
+assign out[0] = (~s) & inp[0];
+assign out[1] = (~s) & inp[1] | (s) & inp[0];
+
+    
+endmodule
+
+//////_TB_//////////
+module Tb ();
+
+reg s = 0;
+reg [1:0]inp;
+wire [1:0]out;
+
+shifter shifter(
+    .s(s),
+    .inp(inp),
+    .out(out)
+);
+
+initial begin
+	 s = 1;
+    inp= 2'b00; #30;
+    inp= 2'b01; #30;
+    inp= 2'b10; #30;
+    inp= 2'b11; #30;
+	 
+	 s = 0;
+	 inp= 3'b11; #30;
+	 
+
+end
+endmodule
