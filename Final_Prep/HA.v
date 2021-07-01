bd7b969568014ef26c8dc2bcbc747dd5b09428fc
Mishka Tenieshvili <59793581+Mishkaten@users.noreply.github.com>
2021-07-01 20:29:45 +0400

    Update HA.v

--- Final_Prep/HA.v
+++ Final_Prep/HA.v
@@ -9,3 +9,31 @@ assign C = (A&B);
 
 
 endmodule
+
+
+
+/////////_TB_////////
+module TB();
+
+
+reg A, B;
+wire Sum, C;
+
+HA HA (
+	A, B,
+	Sum, C
+);
+
+
+initial begin
+
+A = 0; B = 0; #50;
+A = 0; B = 1; #50;
+A = 1; B = 0; #50;
+A = 1; B = 1; #50;
+
+end 
+
+
+endmodule
+
